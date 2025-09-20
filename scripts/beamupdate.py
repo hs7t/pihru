c040e9bda239a56037817c1e42a2fcd1afa79c14
Alí Sinisterra <haidarconh@gmail.com>
2025-09-20 14:43:30 -0500

    beamupdate: rename function to runCommand

--- scripts/beamupdate.py
+++ scripts/beamupdate.py
@@ -1,10 +1,10 @@
 import subprocess
 
-def runOutputCommand(command: list):
+def runCommand(command: list):
     try:
         result = subprocess.run(command, capture_output=True, text=True, check=True)
         return result
     except subprocess.CalledProcessError as e: 
         return "oh no something happened idk"
 
-print(runOutputCommand(['echo', 'hello, world']))
+print(runCommand(['echo', 'hello, world']))
