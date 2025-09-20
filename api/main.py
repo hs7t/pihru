68bf2fa0547e3fb96f71882e08d3529b256cf8a0
Alí Sinisterra <haidarconh@gmail.com>
2025-09-20 16:30:33 -0500

    start writing Beam class

--- api/main.py
+++ api/main.py
@@ -1,1 +1,13 @@
 import fastapi
+import datetime
+
+from pydantic import BaseModel
+from typing import Optional 
+
+class BeamStats(BaseModel):
+    cpuUsage: Optional[object]
+
+
+class Beam(BaseModel):
+    time: datetime.datetime
+    stats: BeamStats
