6ebc94bb44f7d8a00dc6d4764d4dc06b5932126b
Alí Sinisterra <haidarconh@gmail.com>
2025-09-20 15:04:56 -0500

    beamupdate: nuke everything. turns out there was a library :sob:

--- scripts/beamupdate.py
+++ scripts/beamupdate.py
@@ -3,11 +3,3 @@ import subprocess
 def runCommand(command: list):
     result = subprocess.run(command, capture_output=True, text=True, check=True)
     return result
-
-def readTemperature():
-    try: 
-        commandOutput = runCommand(['vcgencmd', 'measure_temp']).stdout
-    except subprocess.CalledProcessError as e:
-        raise e
-    
-    
