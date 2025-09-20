2951c7ef7a01228bd60fb241c6f86d4645817a46
Alí Sinisterra <haidarconh@gmail.com>
2025-09-20 13:59:32 -0500

    beamupdate: add shell=true to readEcho test

--- scripts/beamupdate.py
+++ scripts/beamupdate.py
@@ -2,8 +2,8 @@ import subprocess
 
 def readEcho():
     try:
-        result = subprocess.run(['echo', '"hello, world'], capture_output=True, text=True, check=True)
-        return result
+        result = subprocess.run(['echo', 'hello world'], capture_output=True, text=True, check=True, shell=True)
+        return result.stdout
     except subprocess.CalledProcessError as e: 
         return "oh no something happened idk"
 
