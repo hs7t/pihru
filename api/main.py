7a74218ee9c4186a932768c8f1f2418b372a875d
Alí Sinisterra <haidarconh@gmail.com>
2025-09-21 17:58:34 -0500

    api/main: fastapi follow tutorial for security

--- api/main.py
+++ api/main.py
@@ -1,8 +1,10 @@
 import datetime
 from data import readBeams, insertBeam
 
-from fastapi import FastAPI, Depends
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+from passlib.context import CryptContext
 
 from typing import Annotated
 from pydantic import BaseModel
@@ -10,6 +12,42 @@ from pydantic import BaseModel
 app = FastAPI()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+fake_users_db = {
+    "june": {
+        "username": "june",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    }
+}
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+class User(BaseModel):
+    username: str
+    disabled: bool | None = None
+
+class UserInDB(User):
+    hashed_password: str
 
 class BeamStats(BaseModel):
     CPUUsage: dict | None
@@ -29,7 +67,18 @@ async def logBeam(beam: Beam):
 async def getLatestBeam():
     return readBeams()[-1]
 
-@app.get("/secretestsecret/")
-async def uncoverSecret(token: Annotated[str, Depends(oauth2_scheme)]):
-    return {"secret": "i like cats", "token": "token"}
-
+@app.get("/token/")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    
+    return {
+        "access_token": user.username,
+        "token_type": "bearer"
+    }
